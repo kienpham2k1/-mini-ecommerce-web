6661bd8a92677248ce76e85eda50b81114bbe237
Kien <phamthanhkien2k1@gmail.com>
2022-10-13 19:29:59 +0700

    Init Spring boot project

---
+++ src/main/java/com/springboot/miniecommercewebapp/MiniEcommerceWebappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.miniecommercewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniEcommerceWebappApplication.class, args);
+    }
+
+}
